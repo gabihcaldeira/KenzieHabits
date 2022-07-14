606f146ce0dd493e8d9f2ab7b584651554b7e753
Gabriela Caldeira <gabihcaldeira@hotmail.com>
2022-07-14 18:52:17 -0300

    fix: remove console.log

--- src/scripts/dashboard.scripts.js
+++ src/scripts/dashboard.scripts.js
@@ -49,11 +49,11 @@ CreateEditProfile.botaoModalEdit.addEventListener('click', (event) => {
     CreateEditProfile.modal.classList.remove('close__buttonCreate')
 })
 
-console.log(CreateEditProfile.botaoSair)
+// console.log(CreateEditProfile.botaoSair)
 
 CreateEditProfile.botaoSair.addEventListener('click', (event) => {
 
-    
+
 
     event.preventDefault()
     CreateEditProfile.modal.classList.add('close__buttonCreate')
