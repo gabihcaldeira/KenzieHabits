173e849a517c086cd1ce53786b2e9530775b196e
Arthur Ferreira <arthur13042001@gmail.com>
2022-07-14 10:05:05 -0300

    fix: import and export editModal

--- src/models/edit.models.js
+++ src/models/edit.models.js
@@ -1,28 +1,37 @@
 import ApiRequest from '../controller/Api.controller.js'
 
 
-const modal = document.querySelector('.modal')
-const botaoSair = document.querySelector('.modal__inner--button')
-botaoSair.addEventListener('click',()=>modal.classList.add('display-none'))
+
 export default class EditModal{
 
     static form = document.querySelector('.modal__inner--form')
     static formElements = [...this.form.elements]
 
+
     static async updateHabit(id){
-         console.log(this.form)
-        const updateHabit = {
-        habit_title: this.formElements[0].value,
-        habit_description:  this.formElements[1].value,
-        habit_category: this.formElements[2].value
-      }
-    const response = await ApiRequest.updateHabit(updateHabit,id)
-    if(response.habit_id){
-        swal("Sucesso", "Seu hábito foi editado", "success");
-    }
-    return updateHabit
-    }
-}
+        console.log(this.form)
+       const updateHabit = {
+       habit_title: this.formElements[0].value,
+       habit_description:  this.formElements[1].value,
+       habit_category: this.formElements[2].value
+     }
+   const response = await ApiRequest.updateHabit(updateHabit,id)
+   if(response.habit_id){
+       swal("Sucesso", "Seu hábito foi editado", "success");
+   }
+   return updateHabit
+   }
+
+
+    static async updateModal(){
+    const modal = document.querySelector('.modal')
+    const botaoSair = document.querySelector('.modal__inner--button')
+    botaoSair.addEventListener('click',()=>modal.classList.add('display-none'))
+
+
+    
+    
+
 
 
 console.log(await ApiRequest.readAllHabits())
@@ -30,7 +39,9 @@ console.log(await ApiRequest.readAllHabits())
 
 const botaoModal = document.querySelectorAll('.modalAbrir')
 
+
 botaoModal.forEach(el => {
+    
         el.addEventListener('click', ()=>{
             const id = el.id;
             console.log(id)
@@ -43,3 +54,17 @@ botaoModal.forEach(el => {
         })
     })
 })
+
+}
+
+
+
+
+
+
+
+
+
+
+
+}
